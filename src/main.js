17ca90099accad0a44873a1201f30bbdfd0c8127
rangari123 <avishkarrangari213@gmail.com>
2023-06-18 20:30:53 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/componenets/header.css";
+import "../styles/componenets/hero.css";
+import "../styles/componenets/about.css";
+import "../styles/componenets/featured.css";
+import "../styles/componenets/work.css";
+import "../styles/componenets/contact.css";
+import "../styles/componenets/footer.css";
+import "../styles/componenets/mobile-nav.css";
+import "../styles/utils.css";
+
+import mobileNav from "./utils/mobile-nav";
+import darkMode from "./utils/dark-mode";
+// import lazyLoading from "./utils/lazy-loading";
+mobileNav();
+darkMode();
+// lazyLoading();
